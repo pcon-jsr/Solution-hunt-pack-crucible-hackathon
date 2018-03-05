9bd231214b585f866c01396b39da31b2ed3b873f
ritesh-nitjsr <ritesh.nitjsr01@gmail.com>
2018-03-05 22:17:13 +0530

    Minor Change 1

--- run_scrapper.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-import scrap_tatainnoverse
-
-
-process = CrawlerProcess( get_project_settings() )
-process.crawl('scrap_challenge_desc')
-process.start()
